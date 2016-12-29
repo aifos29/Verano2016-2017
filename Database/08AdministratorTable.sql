c222fd92401b388fd9cc2917796f2a99336c7130
Nelson <nelsk895@gmail.com>
2016-12-28 23:10:46 -0600

    DataBase
    Scripts for create database

---
+++ Database/08AdministratorTable.sql
@@ -0,0 +1,46 @@
+USE [procedureDB]
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Administrator_logging]') AND parent_object_id = OBJECT_ID(N'[dbo].[Administrator]'))
+ALTER TABLE [dbo].[Administrator] DROP CONSTRAINT [FK_Administrator_logging]
+GO
+
+USE [procedureDB]
+GO
+
+/****** Object:  Table [dbo].[Administrator]    Script Date: 12/28/2016 22:17:58 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Administrator]') AND type in (N'U'))
+DROP TABLE [dbo].[Administrator]
+GO
+
+USE [procedureDB]
+GO
+
+/****** Object:  Table [dbo].[Administrator]    Script Date: 12/28/2016 22:17:58 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Administrator](
+	[idAdministrator] [int] NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[lastName] [nvarchar](50) NOT NULL,
+	[idLogging] [int] NOT NULL,
+ CONSTRAINT [PK_Administrator] PRIMARY KEY CLUSTERED 
+(
+	[idAdministrator] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Administrator]  WITH CHECK ADD  CONSTRAINT [FK_Administrator_logging] FOREIGN KEY([idLogging])
+REFERENCES [dbo].[logging] ([idLogging])
+GO
+
+ALTER TABLE [dbo].[Administrator] CHECK CONSTRAINT [FK_Administrator_logging]
+GO
+
+
---
+++ Database/12PeriodOfAbsence.sql
@@ -0,0 +1,46 @@
+USE [procedureDB]
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_periodOfAbsence_Secretary]') AND parent_object_id = OBJECT_ID(N'[dbo].[periodOfAbsence]'))
+ALTER TABLE [dbo].[periodOfAbsence] DROP CONSTRAINT [FK_periodOfAbsence_Secretary]
+GO
+
+USE [procedureDB]
+GO
+
+/****** Object:  Table [dbo].[periodOfAbsence]    Script Date: 12/28/2016 22:47:50 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[periodOfAbsence]') AND type in (N'U'))
+DROP TABLE [dbo].[periodOfAbsence]
+GO
+
+USE [procedureDB]
+GO
+
+/****** Object:  Table [dbo].[periodOfAbsence]    Script Date: 12/28/2016 22:47:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[periodOfAbsence](
+	[idPeriodOfAbsence] [int] NOT NULL,
+	[from] [date] NOT NULL,
+	[to] [date] NOT NULL,
+	[idSecretary] [int] NOT NULL,
+ CONSTRAINT [PK_periodOfAbsence] PRIMARY KEY CLUSTERED 
+(
+	[idPeriodOfAbsence] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[periodOfAbsence]  WITH CHECK ADD  CONSTRAINT [FK_periodOfAbsence_Secretary] FOREIGN KEY([idSecretary])
+REFERENCES [dbo].[Secretary] ([idSecretary])
+GO
+
+ALTER TABLE [dbo].[periodOfAbsence] CHECK CONSTRAINT [FK_periodOfAbsence_Secretary]
+GO
+
+
