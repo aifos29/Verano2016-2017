c2d306dd11cd443017f7dc64bf687e1cc1db41d9
Nelson <nelsk895@gmail.com>
2017-01-06 18:42:59 -0600

    Intento de subida 3

--- GestorDocumentosEntrada/GestorDocumentosEntrada/Controllers/ProcedureController.cs
+++ GestorDocumentosEntrada/GestorDocumentosEntrada/Controllers/ProcedureController.cs
@@ -36,6 +36,7 @@ namespace GestorDocumentosEntrada.Controllers
         {
             ViewBag.deparmentTable = con.getDepartments();
             ViewBag.platformersTable = con.getPlatformers();
+            ViewBag.searchTable = con.getSearchPlat("Amanda Solano");
             return View();
         }
 
@@ -110,6 +111,15 @@ namespace GestorDocumentosEntrada.Controllers
             return View();
         }
 
-       
+       public ActionResult searchByDate(DateTime from, DateTime to){
+           ViewBag.searchTable = con.getSearchDate(from, to);
+           return View();
+       }
+
+       public ActionResult searchByCode(String code)
+       {
+           ViewBag.searchTable = con.getSearchCode(code);
+           return View();
+       }
     }
 }
